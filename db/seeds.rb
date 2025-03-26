aa52d8c693b6e0e60491b7171189cb6a982f2086
Rodrigo Alvarez de Toledo <alvarezdetoledo14@gmail.com>
2025-03-26 20:28:42 +0000

    ejercicio2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+restaurants = [
+  { name: "Epicure", address: "75008 Paris", category: "french" },
+  { name: "Sushi Yasuda", address: "10021 New York", category: "japanese" },
+  { name: "La Trattoria", address: "Roma, Italia", category: "italian" },
+  { name: "Dim Sum House", address: "10105 Los Angeles", category: "chinese" },
+  { name: "Le Saint-Germain", address: "75006 Paris", category: "french" }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create(restaurant)
+end
